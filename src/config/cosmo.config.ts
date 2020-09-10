193b57cd9959735dd9cbbca42ccbd0f8160258f2
ianseberg <daniele.coassin@gmail.com>
2020-09-10 15:09:00 +0200

    v2.2.9

---
+++ src/config/cosmo.config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    production: false,
+    read_key: 'wUJeTT3a7yhspW6PHiCwRL7TN10CauyBlPUD6WTGn5B1cQD5Eb',
+    write_key: 'RJ2YoQdhA4J25o4kYH5RSwpbAlO7qyqzngA8zacT9b4dQMMhNR',
+    bucket_id: '5f4fd69a6509a10008981fa6',
+    bucket_slug: '1k-sunny',
+    URL: 'https://api.cosmicjs.com/v1/',
+    };
